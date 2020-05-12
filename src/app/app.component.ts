83edcf3a0c473fb70f1fbab633de439aa7f84f2a
Amit Khairnar <amitkhairnar@outlook.com>
2020-05-12 11:02:37 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-multiselect-dropdown';
+export class AppComponent implements OnInit {
+  list : any[];
+  title = 'recipieBook';
+
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
+
+  constructor(){
+    this.list =
+      [
+        {name :'India',checked : false},
+        {name :'US',checked : false},
+        {name :'China',checked : false},
+        {name :'France',checked : false}
+      ]
+  }
+
+  ngOnInit() {
+    this.dropdownList = [
+      { item_id: 1, item_text: 'United States' },
+      { item_id: 2, item_text: 'India' },
+      { item_id: 3, item_text: 'China' },
+      { item_id: 4, item_text: 'Japan' },
+      { item_id: 5, item_text: 'Germany' },
+      { item_id: 6, item_text: 'Australia' },
+      { item_id: 7, item_text: 'United Kingdom' },
+      { item_id: 8, item_text: 'Sweden' },
+      { item_id: 9, item_text: 'Norway' },
+      { item_id: 10, item_text: 'Dubai' }
+    ];
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true,
+      searchPlaceholderText: 'Search'
+    };
+  }
+
+  onItemSelect(item: any) {
+    console.log(item);
+  }
+
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+
+  onSelectedValues(items: any){
+    console.log(items);
+  }
 }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule }      from '@angular/core';
+import { CommonModule }  from '@angular/common';
+import { FormsModule }   from '@angular/forms';
+import { MultiSelectComponent } from './multi-select.component';
+import { ClickOutsideDirective } from './click-outside.directive';
+import { ListFilterPipe } from './list-filter.pipe';
+
+@NgModule({
+  imports: [CommonModule, FormsModule],
+  declarations: [
+    MultiSelectComponent,
+    ClickOutsideDirective,
+    ListFilterPipe
+  ],
+  providers: [],
+  exports: [
+    CommonModule,
+    FormsModule,
+    MultiSelectComponent,
+    ClickOutsideDirective,
+    ListFilterPipe
+  ]
+})
+export class SharedModule { }
